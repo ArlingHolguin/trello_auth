0e1ac805aa6b6775512f4a7837be30e9e29a1b2a
Arlin Holguin <arlingholguin@gmail.com>
2024-02-02 11:33:58 -0500

    Profile

--- src/app/modules/layout/components/navbar/navbar.component.ts
+++ src/app/modules/layout/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   faBell,
   faInfoCircle,
@@ -9,19 +9,20 @@ import {
 //importar el servicio de auth
 import { AuthService } from '@services/auth.service';
 import { Router } from '@angular/router';
+import { User } from '@models/user.model';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
   faBell = faBell;
   faInfoCircle = faInfoCircle;
   faClose = faClose;
   faAngleDown = faAngleDown;
-
   isOpenOverlayAvatar = false;
   isOpenOverlayBoards = false;
+  user: User | null = null;
 
   constructor(
     //inyectar el servicio de auth
@@ -34,5 +35,18 @@ export class NavbarComponent {
     // Llamar al servicio de auth para hacer el logout
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  ngOnInit() {
+    // TODO
+    // Obtener el usuario autenticado
+    this.authService.getProfile().subscribe(
+      (user) => {
+        this.user = user;
+      },
+      (error) => {
+        console.error('Error al obtener el usuario autenticado', error);
+      }
+    );
   }
 }
